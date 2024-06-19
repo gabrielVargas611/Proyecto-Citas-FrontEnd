381182b9d2e2a12bd8d7bdcbb75f2495ae1d579b
gabrielVargas611 <gavargas77@gmail.com>
2024-06-19 17:48:01 -0600

    Programaciones model
    Add it data of programaciones

---
+++ Citas/src/model/programaciones.ts
@@ -0,0 +1,8 @@
+export interface Programaciones{
+    ProgramacionesId: string;
+    FechaInicioDisponible: string;
+    fechaFinalDisponible: string;
+    IdDelServicio: string;
+    FechaDeCreacion: string;
+    ActualizadoEn: string;
+}
