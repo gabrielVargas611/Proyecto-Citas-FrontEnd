aa04e5da64d7278b05e3d992c661bd78f6ae4404
gabrielVargas611 <gavargas77@gmail.com>
2024-06-19 17:48:40 -0600

    Programaciones component
    Add the code that shows what you should see on the web page

--- Citas/src/app/pages/programaciones/programaciones.component.ts
+++ Citas/src/app/pages/programaciones/programaciones.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {Programaciones} from '../../../Model/Programaciones';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-programaciones',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './programaciones.component.html',
   styleUrl: './programaciones.component.css'
 })
 export class ProgramacionesComponent {
-
+  public Titulo = 'Administracion de Programaciones'
+  public Programaciones = signal<Programaciones[]>([
+    {
+      ProgramacionesId: 'test',
+      FechaInicioDisponible: 'test',
+      fechaFinalDisponible: 'test',
+      IdDelServicio: 'test',
+      FechaDeCreacion: 'test',
+      ActualizadoEn: 'test'
+    }
+  ]);
 }
