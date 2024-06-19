c9037b9ad32967258e4ada9e85444277261ab70f
gabrielVargas611 <gavargas77@gmail.com>
2024-06-19 17:12:46 -0600

    app.route creation
    Path add it to the routes

--- Citas/src/app/app.routes.ts
+++ Citas/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ProgramacionesComponent } from './pages/programaciones/programaciones.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { SolicitantesComponent } from './pages/solicitantes/solicitantes.component';
+import { SolicitudesComponent } from './pages/solicitudes/solicitudes.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'programaciones',
+        component: ProgramacionesComponent
+    },
+    {
+        path: 'servicios',
+        component: ServiciosComponent
+    },
+    {
+        path: 'solicitantes',
+        component: SolicitantesComponent
+    },
+    {
+        path: 'solicitudes',
+        component: SolicitudesComponent
+    },
+    {
+        path: 'usuarios',
+        component: UsuariosComponent
+    }
+];
