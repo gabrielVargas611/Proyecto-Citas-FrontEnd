2980e06a92173020de60bd25941e18762fcd8dce
gabrielVargas611 <gavargas77@gmail.com>
2024-06-19 20:02:25 -0600

    Servicios Model
    Add it information in Model of the Servicios

---
+++ Citas/src/model/servicios.ts
@@ -0,0 +1,11 @@
+export interface Servicios{
+    serviciosId: String,
+    nombreDelServicio: String,
+    descripcionDelServicio: String,
+    telefonoDeContacto: String,
+    correoDeContacto: String,
+    precioDelServicio: String,
+    nombreDelExperto: String,
+    FechaDeCreacion: String,
+    ActualizadoEn: String,
+}
